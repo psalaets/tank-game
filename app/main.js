977f5f9e7f3063f4ee788dadc543f1fb13ee0d71
Paul Salaets <psalaets@gmail.com>
2015-10-17 20:09:09 -0400

    watch task

--- app/main.js
+++ app/main.js
@@ -1,12 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var a = <div>hello there!</div>;
-
+var a = <h1>hello world</h1>;
 
 var header = document.querySelector('[data-message]')
-console.dir(header)
+console.dir(header);
 
-ReactDOM.render(a, header)
+ReactDOM.render(a, header);
 
-console.log('test')
+console.log('test');
--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,45 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
+var watchify = require('watchify');
+var babelify = require('babelify');
+
+var xtend = require('xtend');
+
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
 var browserSync = require('browser-sync').create();
 
 gulp.task('build-js', function() {
   return makeBundle();
 });
 
-function makeBundle() {
-  var bundler = browserify('app/main.js', {
-    // debug: true
-  });
+gulp.task('watch-js', function() {
+  return makeBundle(true);
+});
 
-  return bundler
-    .transform('babelify')
-    .bundle()
-    // convert regular node stream into gulp compatible stream
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('build'));
+function makeBundle(watch) {
+  var browserifyArgs = xtend({debug: true}, watchify.args);
+  var bundler = browserify(browserifyArgs);
+
+  if (watch) {
+    bundler = watchify(bundler);
+    bundler.on('update', rebundle);
+  }
+
+  bundler.add('app/main.js');
+  bundler.transform(babelify)
+
+  function rebundle() {
+    return bundler
+      .bundle()
+      // convert regular node stream into gulp compatible stream
+      .pipe(source('bundle.js'))
+      .pipe(buffer())
+      .pipe(gulp.dest('build'));
+  }
+
+  return rebundle();
 }
 
 gulp.task('build-html', function() {
@@ -25,7 +47,7 @@ gulp.task('build-html', function() {
     .pipe(gulp.dest('build'));
 });
 
-gulp.task('watch', ['build-js', 'build-html'], function(cb) {
+gulp.task('watch', ['watch-js', 'build-html'], function(cb) {
   browserSync.init({
     server: {
       baseDir: './',
@@ -34,7 +56,6 @@ gulp.task('watch', ['build-js', 'build-html'], function(cb) {
     open: false,
     ghostMode: false
   }, function() {
-    gulp.watch('app/**/*.js', ['build-js']);
     gulp.watch('app/**/*.html', ['build-html']);
     gulp.watch('build/**/*', browserSync.reload);
 
