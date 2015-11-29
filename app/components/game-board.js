abda5f0989de8e716f17a71e951a7dada5569b22
Paul Salaets <psalaets@gmail.com>
2015-11-28 22:34:02 -0500

    not needed, css will handle this

--- app/components/game-board.js
+++ app/components/game-board.js
@@ -19,7 +19,7 @@ var GameBoard = React.createClass({
     });
 
     return (
-      <svg width="100%" transform="scale(0.4)">
+      <svg transform="scale(0.4)">
         <SpriteDefs sprites={this.props.sprites}/>
         {tanks}
       </svg>
