5b9a840d9a43bbd4a76eccbab8e9c854428efe29
Paul Salaets <psalaets@gmail.com>
2016-01-23 14:33:51 -0500

    command factory

---
+++ app/commands.js
@@ -0,0 +1,64 @@
+module.exports = {
+  join,
+  turretThrottle,
+  startFiring,
+  stopFiring,
+  leftThrottle,
+  rightThrottle
+};
+
+var roles = ['watcher', 'driver', 'gunner'];
+
+function join(role) {
+  if (roles.indexOf(role) == -1) {
+    throw new Error('unknown role: ' + role);
+  }
+
+  return {
+    type: 'join',
+    data: {
+      role
+    }
+  };
+}
+
+function turretThrottle(power) {
+  return {
+    type: 'turret-throttle',
+    data: {
+      power
+    }
+  };
+}
+
+function startFiring() {
+  return {
+    type: 'start-firing',
+    data: {}
+  };
+}
+
+function stopFiring() {
+  return {
+    type: 'stop-firing',
+    data: {}
+  };
+}
+
+function leftThrottle(power) {
+  return {
+    type: 'left-throttle',
+    data: {
+      power
+    }
+  };
+}
+
+function rightThrottle(power) {
+  return {
+    type: 'right-throttle',
+    data: {
+      power
+    }
+  };
+}
